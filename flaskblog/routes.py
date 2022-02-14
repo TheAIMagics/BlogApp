b19cdb1947bdf8f9d6b6e280c23822f6f3ccd809
satyazmx <thakursatya6@gmail.com>
2022-02-14 22:40:30 +0530

    pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -141,7 +141,6 @@ def delete_post(post_id):
 
 @app.route('/user/<string:username>')
 def user_posts(username):
-    print(username)
     page = request.args.get('page',1, type= int)
     user = User.query.filter_by(username = username).first_or_404()
     posts = Post.query.filter_by(author = user)\
