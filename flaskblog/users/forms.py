755aab070d260f3f05cd0675570156a412a2d819
satyazmx <thakursatya6@gmail.com>
2022-02-16 09:14:52 +0530

    Blueprints created

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import User
 from flask_login import current_user
-
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -55,11 +54,6 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
@@ -69,7 +63,7 @@ class RequestResetForm(FlaskForm):
         if user is None:
             raise ValidationError('There is no account with this email, Please register first')
 
-class ResetPassowrdForm(FlaskForm):
+class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
